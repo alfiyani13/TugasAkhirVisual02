35a5645a4b3602c5580c3a6b889e018a438fe813
alfiyani13 <ahmadalfiyani999@gmail.com>
2023-06-29 21:58:15 +0800

    upload kedua

--- visual2.sql
+++ visual2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Jun 2023 pada 16.07
+-- Waktu pembuatan: 28 Jun 2023 pada 17.09
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
